5edbe8e2c054054002ea3d53c2e657a8c32167e8
eerrad213 <eerrad213@gmail.com>
2014-10-28 15:38:19 -0400

    Added all information needed for adding a user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ActiveRecord::Base
+	has_secure_password
+
+	def user_params
+      params.require(:user).permit(:email, :password, :password_confirmation)
+    end
+
+	validates_uniqueness_of :email
 end
