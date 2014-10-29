093b6491ee681fb26f62f0826f224c6b9c3c2f2d
eerrad213 <eerrad213@gmail.com>
2014-10-29 11:53:21 -0400

    Added an option for a user to delete comments.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,6 +6,13 @@ class CommentsController < ApplicationController
 		redirect_to card_path(@card)
 	end
 
+	def destroy
+		@card = Card.find(params[:card_id])
+		@comment = @card.comments.find(params[:id])
+		@comment.destroy
+		redirect_to card_path(@card)
+	end
+
 	private
 	def comment_params
 		params.require(:comment).permit(:commenter, :body)
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,7 +1,6 @@
 class Card < ActiveRecord::Base
 
-	has_many :comments
-
+	has_many :comments, dependent: :destroy
 	validates :subject, presence: true,
 		length: { minimum: 5 }
 end
