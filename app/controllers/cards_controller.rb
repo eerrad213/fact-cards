f452e3d3a802aefd5fdc9b5acc3ecfbd129aea88
eerrad213 <eerrad213@gmail.com>
2014-10-29 13:10:29 -0400

    Commented an alternative Basic Authentication.

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,5 +1,7 @@
 class CardsController < ApplicationController
 
+	# http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+
 	before_filter :authorize, only: [:edit, :update, :new]
 
 	def index
